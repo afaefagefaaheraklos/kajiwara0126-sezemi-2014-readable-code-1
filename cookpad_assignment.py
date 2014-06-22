54de495d8d9fc4932aca6e2125760efd7efcd885
Katsumi Takano <hunzyliam@gmail.com>
2014-06-22 13:57:12 +0900

    refactor: 不要な関数を削除

--- cookpad_assignment.py
+++ cookpad_assignment.py
@@ -1,15 +1,4 @@
-def display_recipe():
-	print("ome-rice")
-
-def display_recipe_from_file():
-	recipe_name = open('recipe-data.txt')
-	print(recipe_name.read(), end="")
-	recipe_name.close()
-
-def display_recipes_from_file():
-	recipes_name = open('recipes-data.txt')
-	print(recipes_name.read(), end="")
-	recipes_name.close()
+# coding: utf-8
 
 if __name__ == "__main__":
 	file_name = input("input the file_name, you want to use > ")
