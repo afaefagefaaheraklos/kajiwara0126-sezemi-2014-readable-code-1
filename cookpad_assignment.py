825872667b3602e649c96b2484b682e0566b5538
Katsumi Takano <hunzyliam@gmail.com>
2014-06-22 13:39:37 +0900

    fixed type miss
    and add the memo.md

--- cookpad_assignment.py
+++ cookpad_assignment.py
@@ -3,25 +3,25 @@
 def display_recipe():
 	print("ome-rice")
 
-def display_reccipe_from_file():
+def display_recipe_from_file():
 	recipe_name = open('recipe-data.txt')
-	print(recipe_name.read, end="")
+	print(recipe_name.read(), end="")
 	recipe_name.close()
 
 def display_recipes_from_file():
 	recipes_name = open('recipes-data.txt')
-	print(recipes_name.read, end="")
+	print(recipes_name.read(), end="")
 	recipes_name.close()
 
 if __name__ == "__main__":
 	spec = input("select the spec you want to use > ")
-	if spec == 1:
+	spec = int(spec)	# inputの型を数値型に変換
+	if   spec == 1:
 		display_recipe()
 	elif spec == 3:
-		diplay_recipe_from_file()
+		display_recipe_from_file()
 	elif spec == 4:
-		display_recipes_from_file():
-	
+		display_recipes_from_file()
 	else:
 		pass
 	
