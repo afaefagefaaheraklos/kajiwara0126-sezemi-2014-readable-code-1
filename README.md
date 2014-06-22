55802d7686e0644b7103fe007b5506b5d675c877
Katsumi Takano <hunzyliam@gmail.com>
2014-06-22 14:40:45 +0900

    spec6: IDを指定してレシピを表示できるように修正

--- README.md
+++ README.md
@@ -6,7 +6,13 @@ kajiwara0126-sezemi-2014-readable-code-1
 ```
 $ python cookpad_assignment.py
 input the file_name, you want to use > recipes-data.txt
->>> 1 ome-rice
->>> 2 oyako-don
->>> 3 annin-tofu
+input the id, you want to show (first == 0) > 
+>>> 0 ome-rice
+>>> 1 oyako-don
+>>> 2 annin-tofu
+
+$ python cookpad_assignment.py
+input the file_name, you want to use > recipes-data.txt
+input the id, you want to show (first == 0) > 1
+>>> 1 oyako-don
 ```
--- cookpad_assignment.py
+++ cookpad_assignment.py
@@ -1,12 +1,21 @@
 # coding: utf-8
 
-if __name__ == "__main__":
-	file_name = input("input the file_name, you want to use > ")
-	selected_recipe = open(file_name, "r")
-
-	recipes = selected_recipe.readlines()
-	uniq_id = 1
-	
+def display_recipes_with_id(recipes):
+	Id = 0
 	for recipe in recipes:
-		print( uniq_id, recipe.rstrip() )
-		uniq_id += 1
+		print( Id, recipe.rstrip() )
+		Id += 1
+
+def display_target_recipe(recipes, Id):
+	Id = int(Id)
+	print( Id, recipes[Id].rstrip() )
+
+if __name__ == "__main__":
+	file_name = input( "input the file_name, you want to use > " )
+	recipes   = open( file_name, "r" ).readlines()
+
+	uniq_id = input("input the id, you want to show (first == 0) > ")
+	if(uniq_id == ""):
+		display_recipes_with_id(recipes)
+	else:
+		display_target_recipe(recipes, uniq_id)
