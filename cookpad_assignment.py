bf4f6bf6563aeaf582392df72850d5200ff57b1a
Katsumi Takano <hunzyliam@gmail.com>
2014-06-22 14:06:11 +0900

    spec5: IDを振ってレシピ名を表示

--- cookpad_assignment.py
+++ cookpad_assignment.py
@@ -4,5 +4,9 @@ if __name__ == "__main__":
 	file_name = input("input the file_name, you want to use > ")
 	selected_recipe = open(file_name, "r")
 
-	recipe = selected_recipe.read()
-	print( recipe )
+	recipes = selected_recipe.readlines()
+	uniq_id = 1
+	
+	for recipe in recipes:
+		print( uniq_id, recipe.rstrip() )
+		uniq_id += 1
