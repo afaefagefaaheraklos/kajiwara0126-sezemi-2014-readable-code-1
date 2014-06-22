f253767610613b9a69c2fb6ac274d8aab85c05d6
kajiwara0126 <kajiwara@mtl.t.u-tokyo.ac.jp>
2014-06-22 11:56:36 +0900

    spec4: implemented

--- cookpad_assignment.py
+++ cookpad_assignment.py
@@ -8,12 +8,20 @@ def display_reccipe_from_file():
 	print(recipe_name.read, end="")
 	recipe_name.close()
 
+def display_recipes_from_file():
+	recipes_name = open('recipes-data.txt')
+	print(recipes_name.read, end="")
+	recipes_name.close()
+
 if __name__ == "__main__":
 	spec = input("select the spec you want to use > ")
 	if spec == 1:
 		display_recipe()
 	elif spec == 3:
 		diplay_recipe_from_file()
+	elif spec == 4:
+		display_recipes_from_file():
+	
 	else:
 		pass
 	
